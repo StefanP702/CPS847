4580d49a3e59f2e10f3f38fcab18920aac9deb2b
Stefan Pribisic <157734111+StefanP702@users.noreply.github.com>
2025-04-11 20:22:57 +0000

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+//import AApp from './App2.vue'; // Assume you have another Vue component
+
+// Mount the first app
+createApp(App).mount('#app');
+
+// Mount the second app
+//createApp(AnotherApp).mount('#another-app');
